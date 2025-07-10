6a6d1bae5a3b86fc1d989646c5660e182b0d4c69
404kevnotfound <mewzok7@yahoo.com>
2025-07-10 10:33:47 -0600

    - Finish tutorial part 2: CardView

---
+++ Scrumdinger-Tutorial/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Student on 7/10/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
