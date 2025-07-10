d8d0906d5a3590be54b5231971dafca550c25032
mewzok <mewzok7@yahoo.com>
2025-07-10 08:59:38 -0600

    - Add tutorial project

---
+++ Scrumdinger-Tutorial/Scrumdinger/MeetingView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Student on 7/10/25.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgressView(value: 10, total: 15)
+    }
+}
+
+#Preview {
+    MeetingView()
+}
---
+++ Scrumdinger-Tutorial/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Student on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
