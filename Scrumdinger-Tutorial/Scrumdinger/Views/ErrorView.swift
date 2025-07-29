30165a60a4b18efe9731b9ae51aa45c42e48a194
404kevnotfound <mewzok7@yahoo.com>
2025-07-29 08:55:27 -0600

    - Complete Tutorial: Handling errors

--- Scrumdinger-Tutorial/Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger-Tutorial/Scrumdinger/Views/ErrorView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("An error has occured!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+                
+            }
+        }
     }
 }
 
+private enum SampleError: Error {
+    case errorRequired
+}
+
 #Preview {
-    ErrorView()
+    ErrorView(errorWrapper: ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error."))
 }
